a350bb708b82954fa8a3ba51826ce0e8a01a49b2
Jon Webb <jon@jonwebb.dev>
2025-06-16 00:46:36 -0400

    generic cli package

---
+++ cli/cli.go
@@ -0,0 +1,214 @@
+// Package cli provides a simple command-line interface framework.
+package cli
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// An Env represents the execution environment for a [Command].
+//
+// M is the type of custom metadata that will be available to Command actions.
+type Env[M any] struct {
+	Err  io.Writer         // error output stream
+	Out  io.Writer         // standard output stream
+	Args []string          // command-line arguments
+	Vars map[string]string // environment variables
+	Meta M                 // custom metadata
+}
+
+// Printf formats and writes a message to the standard output stream.
+func (e Env[M]) Printf(format string, args ...any) {
+	if e.Out != nil {
+		fmt.Fprintf(e.Out, format, args...)
+	}
+}
+
+// Errorf formats and writes an error message to the error output stream.
+func (e Env[M]) Errorf(format string, args ...any) {
+	if e.Err != nil {
+		fmt.Fprintf(e.Err, format, args...)
+	}
+}
+
+func (e Env[M]) hasVar(name string) bool {
+	if e.Vars == nil {
+		return false
+	}
+	_, ok := e.Vars[name]
+	return ok
+}
+
+func (e Env[M]) getVar(name string) string {
+	if e.Vars == nil {
+		return ""
+	}
+	return e.Vars[name]
+}
+
+// DefaultEnv returns an [Env] using the current process's environment.
+//
+// The returned Env will use the [os.Stderr] and [os.Stdout] streams, [os.Args],
+// and environment variables from [os.Environ].
+func DefaultEnv[M any](meta M) Env[M] {
+	vars := make(map[string]string)
+	for _, v := range os.Environ() {
+		key, value, _ := strings.Cut(v, "=")
+		vars[key] = value
+	}
+	return Env[M]{
+		Err:  os.Stderr,
+		Out:  os.Stdout,
+		Args: os.Args,
+		Vars: vars,
+		Meta: meta,
+	}
+}
+
+// An ExitStatus is the result of command execution.
+type ExitStatus int
+
+const (
+	ExitSuccess ExitStatus = 0 // execution succeeded
+	ExitFailure ExitStatus = 1 // execution failed due to an error
+	ExitUsage   ExitStatus = 2 // execution failed due to invalid user input
+)
+
+var (
+	errMissingCommand = errors.New("missing command")
+	errUnknownCommand = errors.New("unknown command")
+)
+
+// A Command represents a CLI command.
+//
+// T is the type of the target value for configuration storage.
+// M is the type of the metadata provided by the execution [Env].
+type Command[T any, M any] struct {
+	Name        string                                                     // name used to invoke the command.
+	Usage       string                                                     // short usage text
+	Help        string                                                     // long help text
+	SetFlags    func(flags *flag.FlagSet, target T)                        // function for defining flags
+	Vars        map[string]string                                          // map of flag names -> environment variables
+	Action      func(ctx context.Context, env Env[M], target T) ExitStatus // command action
+	Subcommands []Command[T, M]                                            // command subcommands
+
+	fs *flag.FlagSet
+}
+
+func (c *Command[T, M]) flagSet() *flag.FlagSet {
+	if c.fs == nil {
+		c.fs = flag.NewFlagSet(c.Name, flag.ContinueOnError)
+		c.fs.Usage = func() { /* no-op */ }
+		c.fs.SetOutput(io.Discard)
+	}
+	return c.fs
+}
+
+func (c *Command[T, M]) getFlagVar(flagName string) (string, bool) {
+	if c.Vars == nil {
+		return "", false
+	}
+	varName, ok := c.Vars[flagName]
+	return varName, ok
+}
+
+func (c *Command[T, M]) findSubcommand(name string) *Command[T, M] {
+	if c.Subcommands == nil {
+		return nil
+	}
+	for i := range c.Subcommands {
+		if c.Subcommands[i].Name == name {
+			return &c.Subcommands[i]
+		}
+	}
+	return nil
+}
+
+type boolFlag interface {
+	flag.Value
+	IsBoolFlag() bool
+}
+
+// Execute parses command-line arguments from the environment, then either calls
+// the command's action or defers to the specified subcommand's Execute method.
+func (c *Command[T, M]) Execute(ctx context.Context, env Env[M], target T) ExitStatus {
+	if c.SetFlags != nil {
+		c.SetFlags(c.flagSet(), target)
+	}
+
+	if len(env.Args) < 1 {
+		env.Errorf("command requires at least one arg\n")
+		return ExitFailure
+	}
+
+	if err := c.flagSet().Parse(env.Args[1:]); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			env.Printf("usage: %s\n\n%s\n", c.Usage, c.Help)
+			return ExitSuccess
+		}
+		env.Errorf("usage: %s\n%v\n", c.Usage, err)
+		return ExitUsage
+	}
+
+	setByUser := make(map[string]bool)
+	c.flagSet().Visit(func(f *flag.Flag) {
+		setByUser[f.Name] = true
+	})
+
+	var flagErr error
+	c.flagSet().VisitAll(func(f *flag.Flag) {
+		if flagErr != nil {
+			return
+		}
+
+		if _, wasSet := setByUser[f.Name]; wasSet {
+			return
+		}
+
+		varName, hasVar := c.getFlagVar(f.Name)
+		if !hasVar {
+			return
+		}
+
+		if !env.hasVar(varName) {
+			return
+		}
+
+		value := env.getVar(varName)
+		if err := c.flagSet().Set(f.Name, value); err != nil {
+			if fv, ok := f.Value.(boolFlag); ok && fv.IsBoolFlag() {
+				flagErr = fmt.Errorf("invalid boolean value %q for var $%s: %v", value, varName, err)
+			} else {
+				flagErr = fmt.Errorf("invalid value %q for var $%s: %v", value, varName, err)
+			}
+		}
+	})
+	if flagErr != nil {
+		env.Errorf("usage: %s\n%v\n", c.Usage, flagErr)
+		return ExitUsage
+	}
+
+	env.Args = c.flagSet().Args()
+
+	if c.Action != nil {
+		return c.Action(ctx, env, target)
+	}
+
+	if len(env.Args) == 0 {
+		env.Errorf("usage: %s\n%v\n", c.Usage, errMissingCommand)
+		return ExitUsage
+	}
+
+	subCmd := c.findSubcommand(env.Args[0])
+	if subCmd != nil {
+		return subCmd.Execute(ctx, env, target)
+	}
+
+	env.Errorf("usage: %s\n%v\n", c.Usage, errUnknownCommand)
+	return ExitUsage
+}
---
+++ cli/cli_test.go
@@ -0,0 +1,573 @@
+package cli_test
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/jonathonwebb/x/cli"
+)
+
+func ExampleCommand() {
+	type meta struct {
+		build string
+	}
+
+	type values struct {
+		env  string
+		addr string
+	}
+
+	serveCmd := cli.Command[*values, meta]{
+		Name:  "serve",
+		Usage: "serve [flags]",
+		Help:  "flags:\n  -addr",
+		SetFlags: func(flags *flag.FlagSet, target *values) {
+			flags.StringVar(&target.addr, "addr", "", "")
+		},
+		Vars: map[string]string{
+			"addr": "FOO_ADDR",
+		},
+		Action: func(ctx context.Context, env cli.Env[meta], target *values) cli.ExitStatus {
+			fmt.Printf("env=%s\n", target.env)
+			fmt.Printf("addr=%s\n", target.addr)
+			return cli.ExitSuccess
+		},
+	}
+
+	rootCmd := cli.Command[*values, meta]{
+		Name:  "foo",
+		Usage: "foo [flags] command",
+		Help:  "commands:\n  serve\n\nflags:\n  -env",
+		SetFlags: func(flags *flag.FlagSet, target *values) {
+			flags.StringVar(&target.env, "env", "", "")
+		},
+		Subcommands: []cli.Command[*values, meta]{serveCmd},
+	}
+
+	v := values{}
+	status := rootCmd.Execute(context.Background(), cli.Env[meta]{
+		Args: []string{"foo", "-env=dev", "serve"},
+		Vars: map[string]string{
+			"FOO_ADDR": "localhost:8000",
+		},
+	}, &v)
+
+	fmt.Printf("status=%d", status)
+
+	// Output: env=dev
+	// addr=localhost:8000
+	// status=0
+}
+
+type testCommandResults[T any] struct {
+	outbuf string
+	errbuf string
+	status cli.ExitStatus
+	target T
+}
+
+type testCommandOptions[T any, M any] struct {
+	args   []string
+	vars   map[string]string
+	target T
+	meta   M
+}
+
+func executeTestCommand[T any, M any](ctx context.Context, cmd *cli.Command[T, M], opts testCommandOptions[T, M]) testCommandResults[T] {
+	var outbuf, errbuf bytes.Buffer
+	env := cli.Env[M]{
+		Err:  &errbuf,
+		Out:  &outbuf,
+		Args: opts.args,
+		Vars: opts.vars,
+		Meta: opts.meta,
+	}
+	status := cmd.Execute(ctx, env, opts.target)
+	return testCommandResults[T]{
+		outbuf: outbuf.String(),
+		errbuf: errbuf.String(),
+		status: status,
+		target: opts.target,
+	}
+}
+
+func TestDefaultEnv(t *testing.T) {
+	t.Setenv("TEST_ENV_VAR", "test_value")
+
+	type testMeta struct {
+		version string
+	}
+	meta := testMeta{version: "1.0.0"}
+
+	env := cli.DefaultEnv(meta)
+
+	if env.Err != os.Stderr {
+		t.Errorf("env.Err = %v, want %v", env.Err, os.Stderr)
+	}
+
+	if env.Out != os.Stdout {
+		t.Errorf("env.Out = %v, want %v", env.Out, os.Stdout)
+	}
+
+	if env.Args == nil {
+		t.Errorf("env.Args is nil, want non-nil")
+	}
+
+	value, exists := env.Vars["TEST_ENV_VAR"]
+	if !exists {
+		t.Errorf("env.Vars[\"TEST_ENV_VAR\"] does not exist")
+	}
+
+	if value != "test_value" {
+		t.Errorf("env.Vars[\"TEST_ENV_VAR\"] = %q, want %q", value, "test_value")
+	}
+
+	if env.Meta != meta {
+		t.Errorf("env.Meta = %v, want %v", env.Meta, meta)
+	}
+}
+
+func TestEnv_Printf(t *testing.T) {
+	t.Run("with output writer", func(t *testing.T) {
+		var buf bytes.Buffer
+		env := cli.Env[any]{
+			Out: &buf,
+		}
+
+		env.Printf("test %s", "message")
+
+		want := "test message"
+		if got := buf.String(); got != want {
+			t.Errorf("Printf() wrote %q, want %q", got, want)
+		}
+	})
+
+	t.Run("without output writer", func(t *testing.T) {
+		env := cli.Env[any]{
+			Out: nil,
+		}
+
+		// don't panic!
+		env.Printf("test %s", "message")
+	})
+}
+
+func TestEnv_Errorf(t *testing.T) {
+	t.Run("with error writer", func(t *testing.T) {
+		var buf bytes.Buffer
+		env := cli.Env[any]{
+			Err: &buf,
+		}
+
+		env.Errorf("error %s", "message")
+
+		want := "error message"
+		if got := buf.String(); got != want {
+			t.Errorf("Errorf() wrote %q, want %q", got, want)
+		}
+	})
+
+	t.Run("without error writer", func(t *testing.T) {
+		env := cli.Env[any]{
+			Err: nil,
+		}
+
+		// don't panic!
+		env.Errorf("error %s", "message")
+	})
+}
+
+func TestCommand_Execute(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		cmd := &cli.Command[any, any]{
+			Name: "foo",
+			Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[any, any]{
+			args: []string{"foo"},
+		})
+
+		wantStatus := cli.ExitSuccess
+		if res.status != wantStatus {
+			t.Errorf("res.status=%v, want %v", res.status, wantStatus)
+		}
+	})
+
+	t.Run("nil args", func(t *testing.T) {
+		cmd := &cli.Command[any, any]{
+			Name: "foo",
+			Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[any, any]{
+			args: nil,
+		})
+
+		wantStatus := cli.ExitFailure
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+	})
+
+	t.Run("no args", func(t *testing.T) {
+		cmd := &cli.Command[any, any]{
+			Name: "foo",
+			Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[any, any]{
+			args: []string{},
+		})
+
+		wantStatus := cli.ExitFailure
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+	})
+
+	t.Run("nil vars", func(t *testing.T) {
+		type values struct {
+			bar string
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.StringVar(&v.bar, "bar", "default", "")
+			},
+			Vars: map[string]string{
+				"bar": "FOO_BAR",
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo"},
+			vars: nil,
+		})
+
+		wantStatus := cli.ExitSuccess
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		wantValue := "default"
+		if gotValue := v.bar; gotValue != wantValue {
+			t.Errorf("gotValue=%v, want %v", gotValue, wantValue)
+		}
+	})
+
+	t.Run("-h", func(t *testing.T) {
+		cmd := &cli.Command[any, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[any, any]{
+			args: []string{"foo", "-h"},
+		})
+
+		wantStatus := cli.ExitSuccess
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v,want %v", gotStatus, wantStatus)
+		}
+
+		wantOut := "usage: foo [flags]\n\ndoes foo and then bar\n"
+		gotOut := res.outbuf
+		if diff := cmp.Diff(wantOut, gotOut); diff != "" {
+			t.Errorf("output mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("-help", func(t *testing.T) {
+		cmd := &cli.Command[any, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[any, any]{
+			args: []string{"foo", "-help"},
+		})
+
+		wantStatus := cli.ExitSuccess
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		wantOut := "usage: foo [flags]\n\ndoes foo and then bar\n"
+		gotOut := res.outbuf
+		if diff := cmp.Diff(wantOut, gotOut); diff != "" {
+			t.Errorf("output mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("valid flag", func(t *testing.T) {
+		type values struct {
+			bar  string
+			quux bool
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.StringVar(&v.bar, "bar", "", "")
+				flags.BoolVar(&v.quux, "quux", false, "")
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo", "-bar=baz"},
+		})
+
+		wantStatus := cli.ExitSuccess
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		wantValue := "baz"
+		if gotValue := v.bar; gotValue != wantValue {
+			t.Errorf("gotValue=%v, want %v", gotValue, wantValue)
+		}
+	})
+
+	t.Run("invalid flag", func(t *testing.T) {
+		type values struct {
+			bar  string
+			quux bool
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.StringVar(&v.bar, "bar", "", "")
+				flags.BoolVar(&v.quux, "quux", false, "")
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo", "-invalid_arg"},
+		})
+
+		wantStatus := cli.ExitUsage
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+	})
+
+	t.Run("valid env var", func(t *testing.T) {
+		type values struct {
+			bar  string
+			quux bool
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.StringVar(&v.bar, "bar", "", "")
+				flags.BoolVar(&v.quux, "quux", false, "")
+			},
+			Vars: map[string]string{
+				"bar":  "FOO_BAR",
+				"quux": "FOO_QUUX",
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo"},
+			vars: map[string]string{
+				"FOO_BAR": "baz",
+			},
+		})
+
+		wantStatus := cli.ExitSuccess
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		wantValue := "baz"
+		if gotValue := v.bar; gotValue != wantValue {
+			t.Errorf("gotValue=%v, want %v", gotValue, wantValue)
+		}
+	})
+
+	t.Run("invalid env var", func(t *testing.T) {
+		type values struct {
+			bar  uint
+			quux bool
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.UintVar(&v.bar, "bar", 0, "")
+				flags.BoolVar(&v.quux, "quux", false, "")
+			},
+			Vars: map[string]string{
+				"bar":  "FOO_BAR",
+				"quux": "FOO_QUUX",
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo"},
+			vars: map[string]string{
+				"FOO_BAR": "baz",
+			},
+		})
+
+		wantStatus := cli.ExitUsage
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+	})
+
+	t.Run("invalid bool env var", func(t *testing.T) {
+		type values struct {
+			bar  uint
+			quux bool
+		}
+		var v values
+
+		cmd := &cli.Command[*values, any]{
+			Name:  "foo",
+			Usage: "foo [flags]",
+			Help:  "does foo and then bar",
+			SetFlags: func(flags *flag.FlagSet, target *values) {
+				flags.UintVar(&v.bar, "bar", 0, "")
+				flags.BoolVar(&v.quux, "quux", false, "")
+			},
+			Vars: map[string]string{
+				"bar":  "FOO_BAR",
+				"quux": "FOO_QUUX",
+			},
+			Action: func(ctx context.Context, env cli.Env[any], target *values) cli.ExitStatus {
+				return cli.ExitSuccess
+			},
+		}
+		res := executeTestCommand(t.Context(), cmd, testCommandOptions[*values, any]{
+			args: []string{"foo"},
+			vars: map[string]string{
+				"FOO_QUUX": "baz",
+			},
+		})
+
+		wantStatus := cli.ExitUsage
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+	})
+
+	t.Run("missing command", func(t *testing.T) {
+		rootCmd := &cli.Command[any, any]{
+			Name:  "foo",
+			Usage: "foo [flags] command",
+			Help:  "commands:\n  bar\n  baz",
+			Subcommands: []cli.Command[any, any]{
+				{
+					Name:  "bar",
+					Usage: "foo bar",
+					Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+						return cli.ExitSuccess
+					},
+				},
+				{
+					Name:  "baz",
+					Usage: "foo baz",
+					Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+						return cli.ExitSuccess
+					},
+				},
+			},
+		}
+
+		res := executeTestCommand(t.Context(), rootCmd, testCommandOptions[any, any]{
+			args: []string{"foo"},
+		})
+
+		wantStatus := cli.ExitUsage
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		if !strings.Contains(res.errbuf, "missing command") {
+			t.Errorf("error message %q does not contain 'missing command'", res.errbuf)
+		}
+	})
+
+	t.Run("unknown command", func(t *testing.T) {
+		rootCmd := &cli.Command[any, any]{
+			Name:  "foo",
+			Usage: "foo [flags] command",
+			Help:  "commands:\n  bar\n  baz",
+			Subcommands: []cli.Command[any, any]{
+				{
+					Name:  "bar",
+					Usage: "foo bar",
+					Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+						return cli.ExitSuccess
+					},
+				},
+				{
+					Name:  "baz",
+					Usage: "foo baz",
+					Action: func(ctx context.Context, env cli.Env[any], target any) cli.ExitStatus {
+						return cli.ExitSuccess
+					},
+				},
+			},
+		}
+
+		res := executeTestCommand(t.Context(), rootCmd, testCommandOptions[any, any]{
+			args: []string{"foo", "unknown"},
+		})
+
+		wantStatus := cli.ExitUsage
+		if gotStatus := res.status; gotStatus != wantStatus {
+			t.Errorf("gotStatus=%v, want %v", gotStatus, wantStatus)
+		}
+
+		if !strings.Contains(res.errbuf, "unknown command") {
+			t.Errorf("error message %q does not contain 'unknown command'", res.errbuf)
+		}
+	})
+}
